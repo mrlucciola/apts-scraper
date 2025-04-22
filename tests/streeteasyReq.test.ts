a4ceecf11c52fce1b4cd3ca6bd6364bec72907b0
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2025-04-22 12:19:32 -0400

    feat: add multilisting request config for streeteasy api v6

--- tests/streeteasyReq.test.ts
+++ tests/streeteasyReq.test.ts
@@ -17,3 +17,11 @@ test("expired api key", async () => {
   //
   // expect(res.status).toBeGreaterThanOrEqual(300);
 });
+
+test("multilisting - streeteasy v6", async () => {
+  const listingId = 3943463;
+
+  // const res = await fetchListing(`https://streeteasy.com/rental/${listingId}`);
+  //
+  // expect(res.status).toBeGreaterThanOrEqual(300);
+});
